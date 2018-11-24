b82efabe2b925280a2541d9ac2505e7dd82bbf6c
EG-easy <1018eguchi@gmail.com>
2018-11-24 21:02:32 +0900

    add transaction instead of data

--- proofofwork.go
+++ proofofwork.go
@@ -31,7 +31,7 @@ func (pow *ProofOfWork)prepareData(nonce int) []byte{
 	data := bytes.Join(
 		[][]byte{
 			pow.block.PrevBlockHash,
-			pow.block.Data,
+			pow.block.HashTransactions(),
 			IntToHex(pow.block.Timestamp),
 			IntToHex(int64(targetBits)),
 			IntToHex(int64(nonce)),
@@ -46,7 +46,7 @@ func (pow *ProofOfWork) Run()(int, []byte){
 	var hash [32]byte
 	nonce := 0
 
-	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
+	fmt.Printf("Mining a new block")
 	for nonce < maxNonce {
 		data := pow.prepareData(nonce)
 		hash = sha256.Sum256(data)
